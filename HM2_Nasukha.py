2793295805d031fa8153b4c264601f1d765f7be6
Olexii <93524017+oleksii-nas@users.noreply.github.com>
2026-04-22 10:37:08 +0300

    Update HM2_Nasukha.py

--- HM2_Nasukha.py
+++ HM2_Nasukha.py
@@ -1,14 +1,21 @@
 from datetime import date
 import calendar
 
+users = [
+    {"name": "Petro Oleksiyovich", "birthday": date(1990, 4, 25)},
+    {"name": "Volodya Velik", "birthday": date(1985, 4, 27)},
+    {"name": "Zhenya Samokat", "birthday": date(1992, 4, 26)},
+]
 
 def get_birthdays_per_week(users):
     today = date.today()
 
-    # Ініціалізуємо тільки робочі дні
     result = {day: [] for day in calendar.day_name[:5]}
 
-    for name, birthday in users.items():
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"]
+
         birthday_this_year = birthday.replace(year=today.year)
 
         if birthday_this_year < today:
@@ -19,7 +26,6 @@ def get_birthdays_per_week(users):
         if 0 <= delta_days < 7:
             weekday_index = birthday_this_year.weekday()
 
-            # Перенос вихідних
             if weekday_index >= 5:
                 day_name = "Monday"
             else:
@@ -27,14 +33,13 @@ def get_birthdays_per_week(users):
 
             result[day_name].append(name)
 
-    # Прибираємо порожні дні (якщо потрібно)
-    result = {day: names for day, names in result.items() if names}
+    return {day: names for day, names in result.items() if names}
 
-    return result
 
-users = {  "Petro Oleksiyovich": date(1990, 4, 25), 
-           "Volodya Velik": date(1985, 4, 27), 
-           "Zhenya Samokat": date(1992, 4, 26), 
-        } 
-print(get_birthdays_per_week(users))
+def main():
+    result = get_birthdays_per_week(users)
+    print(result)
 
+
+if __name__ == "__main__":
+    print(get_birthdays_per_week(users))
